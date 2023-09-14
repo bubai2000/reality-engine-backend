6c3c997ccb7d776fe2e9313bc7ee06aec70215c9
Soumyadip <nayaksoumyadip@gmail.com>
2023-09-15 00:00:37 +0530

    Integrate expressJS

--- index.js
+++ index.js
@@ -1,71 +1,7 @@
-// import { config } from "dotenv";
-const { config } = require('dotenv');
-const { GoogleAuth } = require('google-auth-library');
-const https = require('https');
-const axios = require('axios');
+const express = require('express');
+const app = express();
+const routes = require('./routes.js');
 
+app.use(express.json(), routes);
 
-config({
-    path: 'environments/.env'
-})
-async function retieveAccessToken() {
-    const auth = new GoogleAuth({ scopes: 'https://www.googleapis.com/auth/cloud-platform' });
-    process.env.ACCESS_TOKEN = await auth.getAccessToken();
-}
-
-const requestBody = {
-    "instances": [
-        {
-            "context": "",
-            "examples": [],
-            "messages": [
-                {
-                    "author": "user",
-                    // "content": `I am going to provide you a sentence. You will tell me if it\'s true or false in 1 word. You will also provide me a supportive article link if available. The sentence is \\"Chandrayan 3 was made by Lord Hanuman\\"`
-                    "content": `I am going to provide you a statement. You will determing if it's true or false. Then you will return me a JSON with 3 fields. First if the statement is true or false in string format, second if it's false then what is closest correct explanation else null, third if there exists any supporting URL else null. The statement is \\ROCOSMOS was built by ISRO\\"`
-                },
-            ]
-        }
-    ],
-    "parameters": {
-        "candidateCount": 1,
-        "maxOutputTokens": 256,
-        "temperature": 0.2,
-        "topP": 0.8,
-        "topK": 40
-    }
-};
-
-
-async function main() {
-    await retieveAccessToken();
-    // const options = {
-    //     method: "POST",
-    //     headers: {
-    //         "Authorization": `Bearer ${process.env.ACCESS_TOKEN}`,
-    //         "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify(requestBody)
-    // };
-
-    // console.log(options);
-
-
-
-    const response = await axios.post(
-        `https://${process.env.API_ENDPOINT}/v1/projects/${process.env.PROJECT_ID}/locations/us-central1/publishers/google/models/${process.env.MODEL_ID}:predict`,
-        requestBody, {
-        headers: {
-            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-            'Content-Type': 'application/json'
-        }
-    }
-    );
-
-    console.log(JSON.parse(response.data.predictions[0].candidates[0].content));
-
-
-
-}
-
-main();
+app.listen(3000);
---
+++ services/palm_service.js
@@ -0,0 +1,57 @@
+// import { config } from "dotenv";
+const { config } = require('dotenv');
+const { GoogleAuth } = require('google-auth-library');
+const axios = require('axios');
+
+
+config({
+    path: 'environments/.env'
+})
+async function retieveAccessToken() {
+    const auth = new GoogleAuth({ scopes: 'https://www.googleapis.com/auth/cloud-platform' });
+    process.env.ACCESS_TOKEN = await auth.getAccessToken();
+}
+
+
+
+async function main({ statement }) {
+    await retieveAccessToken();
+
+    const requestBody = {
+        "instances": [
+            {
+                "context": "",
+                "examples": [],
+                "messages": [
+                    {
+                        "author": "user",
+                        "content": `I am going to provide you a statement. You will determing if it's true or false. Then you will return me a JSON with 3 fields. First if the statement is true or false in string format, second if it's false then what is closest correct explanation else null, third if there exists any supporting URL else null. The statement is \\${statement}\\"`
+                    },
+                ]
+            }
+        ],
+        "parameters": {
+            "candidateCount": 1,
+            "maxOutputTokens": 256,
+            "temperature": 0.2,
+            "topP": 0.8,
+            "topK": 40
+        }
+    };
+
+
+    const response = await axios.post(
+        `https://${process.env.API_ENDPOINT}/v1/projects/${process.env.PROJECT_ID}/locations/us-central1/publishers/google/models/${process.env.MODEL_ID}:predict`,
+        requestBody, {
+        headers: {
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+            'Content-Type': 'application/json'
+        }
+    }
+    );
+    return JSON.parse(response.data.predictions[0].candidates[0].content);
+}
+
+module.exports = {
+    main
+}
